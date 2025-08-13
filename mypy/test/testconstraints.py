43a072d8b709cf394331712d2b8aba581dffb557
Randolf Scholz <randolf.scholz@gmail.com>
2025-08-13 13:10:39 +0200

    reworked star argument checking

--- mypy/test/testconstraints.py
+++ mypy/test/testconstraints.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from mypy.constraints import SUBTYPE_OF, SUPERTYPE_OF, Constraint, infer_constraints
 from mypy.test.helpers import Suite
 from mypy.test.typefixture import TypeFixture
+from mypy.typeops import make_simplified_union
 from mypy.types import Instance, TupleType, UnpackType
 
 
@@ -22,43 +23,42 @@ class ConstraintsSuite(Suite):
 
     def test_basic_type_var_tuple_subtype(self) -> None:
         fx = self.fx
+        # Note: B <: A, so fallback for tuple[A, B] is tuple[A, ...]
+        fallback = Instance(fx.std_tuplei, [fx.a])
+        target = TupleType([fx.a, fx.b], fallback=fallback)
         assert infer_constraints(
             Instance(fx.gvi, [UnpackType(fx.ts)]), Instance(fx.gvi, [fx.a, fx.b]), SUBTYPE_OF
-        ) == [
-            Constraint(type_var=fx.ts, op=SUBTYPE_OF, target=TupleType([fx.a, fx.b], fx.std_tuple))
-        ]
+        ) == [Constraint(type_var=fx.ts, op=SUBTYPE_OF, target=target)]
 
     def test_basic_type_var_tuple(self) -> None:
-        fx = self.fx
-        assert set(
-            infer_constraints(
-                Instance(fx.gvi, [UnpackType(fx.ts)]), Instance(fx.gvi, [fx.a, fx.b]), SUPERTYPE_OF
-            )
-        ) == {
-            Constraint(
-                type_var=fx.ts, op=SUPERTYPE_OF, target=TupleType([fx.a, fx.b], fx.std_tuple)
-            ),
-            Constraint(
-                type_var=fx.ts, op=SUBTYPE_OF, target=TupleType([fx.a, fx.b], fx.std_tuple)
-            ),
-        }
+        # 1. create class A[T](NamedTuple): a: T; b: T; c: T
+        namedtuplei = self.fx.make_type_info("NamedTuple", module_name="typing")
+        ai = self.fx.make_type_info(
+            "A", typevars=[self.fx.t.name], mro=[namedtuplei, self.fx.std_tuplei, self.fx.oi]
+        )
+        # 2. Create a class MyTuple[T](tuple[T, ...])
+        bi = self.fx.make_type_info(
+            "MyTuple", typevars=[self.fx.t.name], mro=[self.fx.std_tuplei, self.fx.oi]
+        )
+
+        infer_constraints(Instance(ai, [self.fx.t]), Instance(bi, [self.fx.t]), SUBTYPE_OF)
 
     def test_type_var_tuple_with_prefix_and_suffix(self) -> None:
         fx = self.fx
+        # Note: B <: A and C <: A, so fallback for tuple[B, C] is tuple[B | C, ...]
+        fallback = Instance(fx.std_tuplei, [make_simplified_union([fx.b, fx.c])])
+        target = TupleType([fx.b, fx.c], fallback=fallback)
         assert set(
             infer_constraints(
+                # GV[T, *TS, S]
                 Instance(fx.gv2i, [fx.t, UnpackType(fx.ts), fx.s]),
                 Instance(fx.gv2i, [fx.a, fx.b, fx.c, fx.d]),
                 SUPERTYPE_OF,
             )
         ) == {
             Constraint(type_var=fx.t, op=SUPERTYPE_OF, target=fx.a),
-            Constraint(
-                type_var=fx.ts, op=SUPERTYPE_OF, target=TupleType([fx.b, fx.c], fx.std_tuple)
-            ),
-            Constraint(
-                type_var=fx.ts, op=SUBTYPE_OF, target=TupleType([fx.b, fx.c], fx.std_tuple)
-            ),
+            Constraint(type_var=fx.ts, op=SUPERTYPE_OF, target=target),
+            Constraint(type_var=fx.ts, op=SUBTYPE_OF, target=target),
             Constraint(type_var=fx.s, op=SUPERTYPE_OF, target=fx.d),
         }
 
